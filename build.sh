700c95a674a25ec6c45b8e9339db19d953a20804
Jorge <joseroman1633@gmail.com>
2026-03-23 22:47:13 -0600

    Quitar auto creacion de superusuario

--- build.sh
+++ build.sh
@@ -5,5 +5,3 @@ pip install -r requirements.txt
 python scripts/ensure_jazzmin_sourcemaps.py
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-
-python manage.py shell -c "from django.contrib.auth import get_user_model; import os; U=get_user_model(); username=os.environ['DJANGO_SUPERUSER_USERNAME']; email=os.environ.get('DJANGO_SUPERUSER_EMAIL',''); password=os.environ['DJANGO_SUPERUSER_PASSWORD']; user, created = U.objects.get_or_create(username=username, defaults={'email': email, 'is_staff': True, 'is_superuser': True}); user.email=email; user.is_staff=True; user.is_superuser=True; user.set_password(password); user.save(); print(f'Superuser listo: {username}')"
