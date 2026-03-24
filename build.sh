9d4d351d03917e0f789831d286d9d826b3df25dd
Jorge <joseroman1633@gmail.com>
2026-03-23 22:33:32 -0600

    Crear superusuario automatico en deploy

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ pip install -r requirements.txt
 python scripts/ensure_jazzmin_sourcemaps.py
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
+python manage.py createsuperuser --noinput || true
